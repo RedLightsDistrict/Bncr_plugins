3edf79c38f6e3e27ae690d7c02b22a089782b54a
RedLightsDistrict <116049685+RedLightsDistrict@users.noreply.github.com>
2023-03-14 11:46:15 +0800

    Add files via upload

---
+++ 举牌.js
@@ -0,0 +1,43 @@
+/**
+ * @author Aming
+ * @name 举牌
+ * @origin 红灯区
+ * @version 1.0.1
+ * @description 任务监控
+ * @rule ^(举牌|jp)([^\n]+)$
+ * @admin true
+ * @public false
+ * @priority 99999
+ * @disable false
+ */
+
+// const request = require('util').promisify(require('request'));
+const request = require('request');
+const path = require('path');
+const fs = require('fs');
+const { randomUUID } = require('crypto');
+
+module.exports = async s => {
+    let jpgURL = `https://juapi.org/api/zt.php?msg=${encodeURIComponent(s.param(2))}`,
+        open = false;
+    ["tgBot", "HumanTG"].includes(s.getFrom()) && (jpgURL = await writeToJpg(jpgURL)), open = true;   /* 存储图片 */
+    s.delMsg(s.getMsgId());
+    await s.reply({
+        type: 'image',
+        msg: jpgURL
+    });
+    open && fs.unlinkSync(jpgURL);
+};
+
+
+async function writeToJpg(url) {
+    const paths = path.join(process.cwd(), `BncrData/public/${randomUUID().split('-').join('') + '.jpg'}`);
+    return new Promise((resolve) => {
+        request(url).pipe(
+            fs.createWriteStream(paths)
+        ).on('finish', () => {
+            resolve(paths);
+        });
+    });
+};
+
